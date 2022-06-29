63603a02ded514eca4d022eb241a1b72927b1a80
Tenzin <tenthinten@gmail.com>
2022-06-29 21:17:38 +0530

    first extensino

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
+console.log("extension is up and running printing from background script")
 chrome.action.onClicked.addListener(buttonClicked);
 function buttonClicked(tab) {
     let msg = {
         txt: "hello"
     }
-    chrome.tabs.sendMessage(tab.id,msg)
+    chrome.tabs.sendMessage(tab.id,msg);
 }
